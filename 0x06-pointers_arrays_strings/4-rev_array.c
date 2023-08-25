bbcbaad69b2ae540adbcd729c5efc44597a84568
Nso Arrey <spencerice149@gmail.com>
2023-08-25 10:59:52 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * @reverse_array: function to perform array reversal.
- *
- * @i: upward counter,
- * @j : download counter.
- * @tmp : temporary variable.
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers
+ * @n: number of elements in array
  */
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i, j, tmp;
 
 	for (i = 0, j = n - 1; i <= n / 2; i++, j--)
 	{
-		int tmp;
 
 		tmp = a[i];
 		a[i] = a[j];
