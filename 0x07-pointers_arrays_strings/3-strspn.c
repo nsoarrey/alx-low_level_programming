63a2028224926b59092195cb0512f940522de83f
Nso Arrey <spencerice149@gmail.com>
2023-08-30 03:02:27 +0100

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
  */
 unsigned int _strspn(char *s, char *accept)
 {
