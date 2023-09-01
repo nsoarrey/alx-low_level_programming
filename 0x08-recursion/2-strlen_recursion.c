3d1535b0ca2e632635e7b93793f0a97e1d3505b6
Nso Arrey <spencerice149@gmail.com>
2023-08-31 19:44:18 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s)
+	{
+		n++;
+		n += _strlen_recursion(s + 1);
+	}
+	return (n);
+}
