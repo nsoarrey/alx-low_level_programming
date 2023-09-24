cf7968cb27bdb081c5292af37c56bd78137adaa2
Nso Arrey <spencerice149@gmail.com>
2023-09-24 04:40:27 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to structure to be freed.
+ * Return: nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
