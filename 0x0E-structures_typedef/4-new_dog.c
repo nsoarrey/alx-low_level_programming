39b80224cdac349e5f1eccbf8b2a8b20e91d75f7
Nso Arrey <spencerice149@gmail.com>
2023-09-24 04:00:25 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-	int len1 = 0, len2 = 0;
+	unsigned long int len1 = 0, len2 = 0;
 	unsigned long int i, j;
 
 	if (name == NULL || owner == NULL)
@@ -28,10 +28,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p);
 		return (NULL);
 	}
-	for (i = 0; i < len1 + 1; i++)
+	for (i = 0; i < len1; i++)
 	{
 		p->name[i] = name[i];
 	}
+	p->name[i] ='\0';
 	p->age = age;
 	p->owner = malloc(len2 + 1);
 	if (!p->owner)
@@ -40,9 +41,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p);
 		return (NULL);
 	}
-	for (j = 0; j < len2 + 1; j++)
+	for (j = 0; j < len2; j++)
 	{
 		p->owner[j] = owner[j];
 	}
+	p->owner[j] = '\0';
 	return (p);
 }
