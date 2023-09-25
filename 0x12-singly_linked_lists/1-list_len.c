2c9e74b41851f26cf0268301ac4eb58e600d04ef
Nso Arrey <spencerice149@gmail.com>
2023-09-25 07:44:20 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * list_len - calculates number of elements in a list.
+ * @h: pointer to list of type list_t.
+ *
+ * Return:
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		count++;
+	}
+	return (count);
+}
