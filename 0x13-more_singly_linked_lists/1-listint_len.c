9ad9c5867bcb6fe96f9e1c76f3daa0103ab2def2
Nso Arrey <spencerice149@gmail.com>
2023-09-27 00:33:10 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * listint_len - number of elements in a list
+ * listint_len - calculates the s number of elements in a list
  * @h: head of list.
  * Return: number of elements.
  *
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count 0;
+	size_t count = 0;
 
 	if (h)
 	{
-		for (;h != NULL; h = h->next)
+		for (; h != NULL; h = h->next)
 		{
 			count++;
 		}
